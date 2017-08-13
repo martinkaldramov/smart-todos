77931eaf6cf856a19a466304d7308bfc4d45b27e
Marto <martinkaldramov1@gmail.com>
2017-08-13 19:19:19 +0300

    Fixed editable item after messing it up

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component{
-  renderItem(isEdited){
-    if(isEdited){
-      return(
-	<div>
-	<input />
-	<button>Update</button>
-	<button>Cancel</button>
-	</div>
-      );
-    }else{
-      return(
-	<div>
-	<p>{this.props.name}</p>
-	<button onClick={() => this.props.editItem(this.props.id)}>Edit</button>
-	<button onClick={() => this.props.removeItem(this.props.id)}>Delete</button>
-	<button onClick={() => this.props.completeItem(this.props.id)}>Complete</button>
-	</div>
-      );
-    }
-  }
   render(){
     return(
       <div>
-	{this.renderItem(this.props.isEdited)}
-      </div> 
+        {this.props.isEdited ? <input /> : <p>{this.props.name}</p>}
+        <button onClick={() => this.props.editItem(this.props.id)}>Edit</button>
+        <button onClick={() => this.props.removeItem(this.props.id)}>Delete</button>
+        <button onClick={() => this.props.completeItem(this.props.id)}>Complete</button>
+      </div>
     )
   }
 }
@@ -35,14 +18,14 @@ class List extends Component{
     const todos = this.props.todos;
     return(
       <div>
-	 {todos.map((item, index) => <TodoItem 
-	  				key={index} 
-					id={index} 
-					name={item.name} 
-					editItem={this.props.editItem}
-					isEdited={item.isEdited}
-					removeItem={this.props.removeItem} 
-					completeItem={this.props.completeItem}/>)}
+	 {todos.map((item, index) => <TodoItem  
+		   			key={index} 
+	           			id={index} 
+	           			name={item.name} 
+	           			editItem={this.props.editItem} 
+	           			isEdited={item.isEdited} 
+	           			removeItem={this.props.removeItem} 
+	           			completeItem={this.props.completeItem}/>)}
       </div> 
     )
   }  
